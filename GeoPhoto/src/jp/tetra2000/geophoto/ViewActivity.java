9ed35782bb5a08f38f8b50896f1e978957013eae
TETRA2000 <sagittariusm25@gmail.com>
2013-11-16 16:12:22 +0900

    ViewActivityを作成

---
+++ GeoPhoto/src/jp/tetra2000/geophoto/ViewActivity.java
@@ -0,0 +1,11 @@
+package jp.tetra2000.geophoto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
