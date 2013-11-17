a77a3610e07a345bdba6a5b6b2075204b9e34d35
TETRA2000 <sagittariusm25@gmail.com>
2013-11-17 14:14:54 +0900

    Triangleクラスを追加

---
+++ GeoPhoto/src/jp/tetra2000/geophoto/stl/Triangle.java
@@ -0,0 +1,10 @@
+package jp.tetra2000.geophoto.stl;
+
+public class Triangle {
+	double a[], b[], c[];
+	public Triangle(double a[],double b[], double c[]) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+}
