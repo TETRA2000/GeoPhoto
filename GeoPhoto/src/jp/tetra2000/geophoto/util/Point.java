d0a30a3356ac419c7d89ee63c9cd428a611e7363
TETRA2000 <sagittariusm25@gmail.com>
2013-11-17 11:24:30 +0900

    位置を表すクラスを作成

---
+++ GeoPhoto/src/jp/tetra2000/geophoto/util/Point.java
@@ -0,0 +1,11 @@
+package jp.tetra2000.geophoto.util;
+
+public class Point
+{
+    public double latitude, longitude;
+
+	public Point(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+}
