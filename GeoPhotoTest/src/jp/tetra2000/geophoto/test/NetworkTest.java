8cdd103713091540f42769a47f934e714e1c674b
TETRA2000 <sagittariusm25@gmail.com>
2013-11-17 13:21:31 +0900

    PointsMapで生成した座標を用いたテストコード作成

--- GeoPhotoTest/src/jp/tetra2000/geophoto/test/NetworkTest.java
+++ GeoPhotoTest/src/jp/tetra2000/geophoto/test/NetworkTest.java
@@ -8,6 +8,7 @@ import org.apache.http.client.ClientProtocolException;
 import jp.tetra2000.geophoto.network.YOLPHeight;
 import jp.tetra2000.geophoto.util.HeightPoint;
 import jp.tetra2000.geophoto.util.Point;
+import jp.tetra2000.geophoto.util.PointsMap;
 import android.test.AndroidTestCase;
 
 public class NetworkTest extends AndroidTestCase {
@@ -30,5 +31,15 @@ public class NetworkTest extends AndroidTestCase {
 		expected.add(new HeightPoint(139.6847782,35.6265008, 30.3));
 		
 		assertEquals(actual, expected);
+	}
+	
+	public void testGetHeightsで高さが取得できるその2() throws ClientProtocolException, IOException {
+		Point base = new Point(139.732293,35.663613);
+		PointsMap map = PointsMap.create(base, 30, 5, 10);
+		
+		ArrayList<HeightPoint> actual = sut.getHeights(map.toPointArray());
+
+		
+		assertEquals(actual.size(), 10*10);
 	}
 }
