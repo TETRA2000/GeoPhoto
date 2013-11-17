270e3413289bb6d1ade0975e2f032c456c3a0196
TETRA2000 <sagittariusm25@gmail.com>
2013-11-17 11:24:48 +0900

    HeightPointを実装

--- GeoPhoto/src/jp/tetra2000/geophoto/util/HeightPoint.java
+++ GeoPhoto/src/jp/tetra2000/geophoto/util/HeightPoint.java
@@ -1,7 +1,21 @@
 package jp.tetra2000.geophoto.util;
 
 public class HeightPoint {
-    public HeightPoint(double latitude, double longitude, double heignt) {
-
-    }
+    public double latitude, longitude, height;
+	
+	public HeightPoint(double latitude, double longitude, double height) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.height = height;
+	}
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		return	o.getClass() == getClass() ||
+				((HeightPoint)o).latitude == latitude &&
+				((HeightPoint)o).longitude == longitude &&
+				((HeightPoint)o).height == height;
+			
+	}
 }
